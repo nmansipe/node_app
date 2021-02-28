302ab3906c1d780a3d3bc06288036ab88ab28c42
NMANSIPE <nmansinpe@gmail.com>
2021-02-28 23:33:39 +0100

    primer commit app node_app

---
+++ src/index.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const path = require("path");
+const exphbs = require("express-handlebars");
+const methodOverride = require("method-override");
+const session = require("express-session");
+const Handlebars = require('handlebars')
+const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access')
+
+// Initiliazations
+const app = express();
+require('./database');
+
+// Settings
+
+app.set("port", process.env.PORT || 3000);
+app.set("views", path.join(__dirname, "views"));
+app.engine(
+  ".hbs",
+  exphbs({
+    defaultLayout: "main",
+    layoutsDir: path.join(app.get("views"), "layouts"),
+    partialsDir: path.join(app.get("views"), "partials"),
+    extname: ".hbs",
+  })
+);
+app.engine('handlebars', exphbs({
+    handlebars: allowInsecurePrototypeAccess(Handlebars)
+}));
+app.set("view engine", ".hbs");
+
+// Middlewares
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
+app.use(
+  session({
+    secret: "mysecretapp",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+// Global Variables
+
+// Routes
+app.use(require("./routes/index"));
+app.use(require("./routes/notes"));
+app.use(require("./routes/users"));
+
+// Static Files
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// Server is Listennig
+app.listen(app.get("port"), () => {
+  console.log("Server on port", app.get("port"));
+});
---
+++ src/routes/notes.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const router = express.Router();
+
+const Note = require('../model/Note');
+
+router.get('/notes/add', (req,res) => {
+    res.render('notes/new-note');
+})
+
+router.post('/notes/new-note', async (req, res) => {
+    const { title, description }= req.body;
+    const errors = [];
+    if (!title) {
+        errors.push ({text: 'Please Write a Title'});
+    }
+    if (!description) {
+        errors.push({text: 'Please Write a Description'});
+    }
+    if(errors.length > 0) {
+        res.render('notes/new-note', {
+            errors,
+            title,
+            description
+        });
+    } else {
+       const newNote = new Note({ title, description });
+       await newNote.save();
+       res.redirect('/Notes');
+    }    
+});
+
+router.get('/notes', async (req, res) => {
+    const notes = await Note.find();
+    res.render('notes/all-notes', { notes });
+});
+module.exports = router;
